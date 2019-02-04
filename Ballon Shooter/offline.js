3ccf15363865f14b718648837f42fa461e32478b
Rufaidah <rufaidah.chan@gmail.com>
2019-02-04 23:14:50 +0700

    done

---
+++ Ballon Shooter/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1490099568,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/rumput-sheet0.png",
+		"images/tali-sheet0.png",
+		"images/balon-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/karakter-sheet0.png",
+		"images/biru.png",
+		"images/hijau.png",
+		"images/merah.png",
+		"images/kuning.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
